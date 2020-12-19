277a44093bc6af46269879b98b536911bd46a12a
nikanika0221 <60596671+nikanika0221@users.noreply.github.com>
2020-12-19 16:54:48 +0400

    Add files via upload

---
+++ decoder_module.v
@@ -0,0 +1,102 @@
+
+module decoder_module(
+   input [31:0] instr,
+	input clk,
+	input [3:0] flag,
+	output reg [1:0] op,
+	output reg [3:0] bits,
+	output reg [23:0] imminstr,
+	output reg [3:0] base,
+	output reg [3:0] data_reg,
+	output reg [11:0] imminstr_mem,
+	output reg jmp_en,
+	output reg regjmp_en,
+	output reg flag_en,
+	output reg write_data,
+	output reg memory_data,	
+	output reg memdata,
+	output reg memdata_en
+    );
+	 
+	 reg m, n, k;
+	 
+	 always @ (posedge clk)
+	 begin
+		bits <= instr [24:21];
+		op <= instr [27:26];
+		m <= instr [20];	
+		n <= instr [20];	
+		k <= instr [25];
+		case (op)
+		0:
+		begin
+			jmp_en <= 0;	
+			imminstr <= 0;	
+			regjmp_en <= 0;	
+			base <= instr [19:16];
+			data_reg <= instr [15:12];	
+			memory_data <= 0;	
+			imminstr_mem <= 0;
+			memdata <= 0;
+			memdata_en <= 0;
+			if (m == 1) flag_en <= 1;
+			else flag_en <= 0;
+			if (bits == 10) write_data <= 0;
+			else write_data <= 1;
+		end
+		1:
+		begin
+			jmp_en <= 0;
+			regjmp_en <= 0;
+			imminstr <= 0;
+			flag_en <= 0;
+			write_data <= 0;
+			base <= instr [19:16];
+			data_reg <= instr [15:12];
+			memdata_en <= 1;
+			if (n == 1)
+			begin
+				memory_data <= 1;
+				memdata <= 0;
+				memdata_en <= 1;
+			end
+			else
+			begin
+				memory_data <= 0;
+				memdata <= 1;
+				memdata_en <= 0;
+			end
+			if (k == 1) imminstr_mem <= instr [11:0];	
+			else imminstr_mem <= 0;
+		end
+		2:
+		begin
+			jmp_en <= 1;
+			regjmp_en <= 1;
+			imminstr <= instr [23:0];
+			flag_en <= 0;
+			write_data <= 0;
+			base <= 0;
+			data_reg <= 0;
+			memory_data <= 0;
+			imminstr_mem <= 0;
+			memdata <= 0;
+			memdata_en <= 0;			
+		end
+		3:
+		begin
+			jmp_en <= 0;
+			regjmp_en <= 0;
+			imminstr <= 0;
+			flag_en <= 0;
+			write_data <= 0;
+			base <= 0;
+			data_reg <= 0;
+			memory_data <= 0;	
+			imminstr_mem <= 0;
+			memdata <= 0;
+			memdata_en <= 0;
+		end
+	endcase
+	 end
+endmodule
---
+++ testbench.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:14:32 12/19/2020
+// Design Name:   decoder_module
+// Module Name:   D:/Desktop/Xilinx ISE Design Suite 14.5/decoder/testbench.v
+// Project Name:  decoder
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: decoder_module
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module testbench;
+
+	// Inputs
+	reg [31:0] instr;
+	reg clk;
+	reg [3:0] flag;
+
+	// Outputs
+	wire [1:0] op;
+	wire [3:0] bits;
+	wire [23:0] imminstr;
+	wire [3:0] base;
+	wire [3:0] data_reg;
+	wire [11:0] imminstr_mem;
+	wire jmp_en;
+	wire regjmp_en;
+	wire flag_en;
+	wire write_data;
+	wire memory_data;
+	wire memdata;
+	wire memdata_en;
+
+	// Instantiate the Unit Under Test (UUT)
+	decoder_module uut (
+		.instr(instr), 
+		.clk(clk), 
+		.flag(flag), 
+		.op(op), 
+		.bits(bits), 
+		.imminstr(imminstr), 
+		.base(base), 
+		.data_reg(data_reg), 
+		.imminstr_mem(imminstr_mem), 
+		.jmp_en(jmp_en), 
+		.regjmp_en(regjmp_en), 
+		.flag_en(flag_en), 
+		.write_data(write_data), 
+		.memory_data(memory_data), 
+		.memdata(memdata), 
+		.memdata_en(memdata_en)
+	);
+
+	always #1 clk = !clk;
+	initial begin
+		// Initialize Inputs
+		instr = 0;
+		clk = 0;
+		flag = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100; instr = 32'b11010011010010010110111111111111;	
+		#100; instr = 32'b10010011111001011010011111111111;	
+		#100; instr = 32'b11010111111100001000111011000000;	
+		#100; instr = 32'b10010101111011000001000011111111;	
+		#100; instr = 32'b11101011101010101010101010101010;	
+		#100; instr = 32'b11011011010101010101010101010101;	
+	end 
+endmodule
+
